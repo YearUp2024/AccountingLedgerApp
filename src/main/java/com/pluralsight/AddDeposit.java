3595c97daa8c69a20e4e348cfddef452d5cdc21c
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-12 11:17:24 -0400

    HomeScreen Class calling AddDeposit Class

--- src/main/java/com/pluralsight/AddDeposit.java
+++ src/main/java/com/pluralsight/AddDeposit.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class AddDeposit {
+    public static void main() {
+        System.out.println("Add Deposit");
+    }
 }
--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HomeScreen {
+    static Scanner scanner = new Scanner(System.in);
+    static AddDeposit addDeposit = new AddDeposit();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Options();
+    }
+
+    public static void Options(){
+        String userChoice = "";
+        while(!userChoice.equalsIgnoreCase("E")) {
+            System.out.println("Add [D]eposit");
+            System.out.println("Make [P]ayment");
+            System.out.println("[L]edger");
+            System.out.println("[E]xit");
+            System.out.print("Choose from the Options: ");
+            userChoice = scanner.nextLine();
+
+            if(userChoice.equalsIgnoreCase("D")){
+                System.out.println("------------------------------");
+                addDeposit.main();
+                System.out.println("------------------------------");
+            }
+            if(userChoice.equalsIgnoreCase("E")){
+                System.out.println("------------------------------");
+                System.out.println("       Program Ended          ");
+                System.out.println("------------------------------");
+            }
+        }
     }
 }
